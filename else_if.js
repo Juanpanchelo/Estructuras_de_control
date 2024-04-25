7ce1a994d058f1cfbbc2df081d45a60233a5a59a
Juan Jose Alvarez <jjchelo665@gmail.com>
2024-04-25 12:44:13 -0500

    Ejercicio numero 1

---
+++ else_if.js
@@ -0,0 +1,13 @@
+const calificacion = 85;
+if (calificacion >= 90) {
+console.log("Excelente");
+} else if (calificacion >= 80) {
+console.log("Muy bien");
+} else if (calificacion >= 70) {
+console.log("Bien");
+} else {
+console.log("Aprobado");
+}
+//Sentencias condicionales4En este ejemplo, se evalúan las condiciones calificacion >= 90 , calificacion >=
+//80 y calificacion >= 70 . Si se cumple alguna de ellas, se ejecuta el bloque de
+//código correspondiente. Si ninguna de las condiciones se cumple, se ejecuta
---
+++ sentencias-condicionales/ejercicio1.js
@@ -0,0 +1,11 @@
+const numero = prompt("Ingrese un numero");
+parseInt(numero)
+
+if (numero > 0) {
+    alert("El numero es positivo")
+}
+if (numero == 0) {
+        alert("El numero es cero")
+} else {
+    alert("Este numero es negativo")
+}
