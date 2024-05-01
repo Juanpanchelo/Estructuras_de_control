08e1480e5cd2851bdf4dd15718ae76ea58c86e5c
Juan Jose Alvarez <jjchelo665@gmail.com>
2024-04-30 19:33:00 -0500

    Ciclos introduccion

---
+++ Ciclos/while.js
@@ -0,0 +1,17 @@
+// Suponiendo que tenemos una variable que indica la cantidad de agua en la manguera
+let aguaEnManguera = 10; // (Valor inicial de 10 litros
+//de agua)
+i = 5
+i
+Ciclos4// Ciclo while para regar las plantas
+while (aguaEnManguera > 0) {
+console.log("Regando las plantas...");
+// Simulamos el uso de agua para regar
+aguaEnManguera--;
+// Verificamos si aún hay agua en la manguera y si ya
+//no hay entonces se imprime en la consola que se ha acaba
+//do
+if (aguaEnManguera === 0) {
+console.log("¡Se ha acabado el agua!");
+ }
+}
